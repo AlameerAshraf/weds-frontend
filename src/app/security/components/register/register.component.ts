362b0faef304a8238d1664680df054c16186257f
alameerashraf <alamiir.ashraf@gmail.com>
2021-03-15 01:15:56 +0200

    finalize register component UI and Angular

--- src/app/security/components/register/register.component.ts
+++ src/app/security/components/register/register.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-login',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: any,
+  private elementRef: ElementRef , private actictedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-  }
+  };
+
+  ngAfterViewInit(): void {
+    const s = this.document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'assets/scripts/custom.js';
+    this.elementRef.nativeElement.appendChild(s);
+  };
 
 }
