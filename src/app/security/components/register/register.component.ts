f8b4329c5e0d21b73ade06128e85e972898b9fdb
alameerashraf <alamiir.ashraf@gmail.com>
2021-03-17 17:23:55 +0200

    Security register pages for user and vendor

--- src/app/security/components/register/register.component.ts
+++ src/app/security/components/register/register.component.ts
@@ -1,6 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, Inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +8,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit, AfterViewInit {
+  userRegisterationImage = "assets/images/backgrounds/login/8.png";
+  vendorRegisterationImage = "assets/images/backgrounds/login/4_vendor.png";
+  bkImage = "";
+  isVendorRegistering: boolean;
 
-  constructor(@Inject(DOCUMENT) private document: any,
+  constructor(@Inject(DOCUMENT) private document: any, private router: Router,
   private elementRef: ElementRef , private actictedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.actictedRoute.queryParams.subscribe((params) => {
+      let isVendor = this.isVendorRegistering = params["vendor"];
+      this.bkImage = isVendor == undefined ? this.userRegisterationImage : this.vendorRegisterationImage;
+    })
   };
 
   ngAfterViewInit(): void {
@@ -22,4 +30,8 @@ export class RegisterComponent implements OnInit, AfterViewInit {
     this.elementRef.nativeElement.appendChild(s);
   };
 
+  changeRegisterationType(type){
+    if(type == "user") window.location.href = "/security/ar/register";
+    else window.location.href = `/security/ar/register?vendor=join`
+  };
 }
