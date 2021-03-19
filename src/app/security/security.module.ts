9b92fc92bf07f5cddfba265e474ca5fe6fe67500
alameerashraf <alamiir.ashraf@gmail.com>
2021-03-19 07:41:11 +0200

    Adding generic fields validators for any field in the app

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SecurityRoutingModule } from './security-routing.module';
 
 import { COMPONENTS, THEME_MODULE } from './security.imports';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,8 @@ import { COMPONENTS, THEME_MODULE } from './security.imports';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     SecurityRoutingModule,
     THEME_MODULE
   ]
