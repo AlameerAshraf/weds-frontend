a358e979129475dcf808c592e01c7b4910ca9f27
alameerashraf <alamiir.ashraf@gmail.com>
2021-03-20 02:08:00 +0200

    redirect to reset password email view from login

--- src/app/security/security-routing.module.ts
+++ src/app/security/security-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: securityComponents.LoginComponent
+  },
+  {
+    path: 'reset-password',
+    component: securityComponents.ResetPasswordComponent
   }
 ];
 
