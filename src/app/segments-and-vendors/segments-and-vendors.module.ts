87b9f869bf32cf9024cd4b3d308e2d37e378f5c9
alameerashraf <alamiir.ashraf@gmail.com>
2021-03-28 03:10:05 +0200

    Create main segments and landing modules

---
+++ src/app/segments-and-vendors/segments-and-vendors.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SegmentsAndVendorsRoutingModule } from './segments-and-vendors-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SegmentsAndVendorsRoutingModule
+  ]
+})
+export class SegmentsAndVendorsModule { }
