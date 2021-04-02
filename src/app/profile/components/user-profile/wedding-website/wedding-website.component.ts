cb7147a8ab21875845dd65c9c1e7c6263e38a63a
alameerashraf <alamiir.ashraf@gmail.com>
2021-04-02 05:20:44 +0200

    Adding wedding websites template

--- src/app/profile/components/user-profile/wedding-website/wedding-website.component.ts
+++ src/app/profile/components/user-profile/wedding-website/wedding-website.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./wedding-website.component.scss'],
 })
 export class WeddingWebsiteComponent implements OnInit {
-
+  is = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectTemplate(e){
+    e.preventDefault();
+    this.is = !this.is;
+    let like = document.getElementById('template1');
+    if(!this.is){
+      like.classList.add("liked");
+    } else {
+      like.classList.remove("liked");
+    }
+  }
 }
