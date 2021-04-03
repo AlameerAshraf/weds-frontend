56ded85e97b4e225eb51c44e97c5f7bee94a0605
alameerashraf <alamiir.ashraf@gmail.com>
2021-04-03 07:57:24 +0200

    Fix some issues and enhancements in area

--- src/app/profile/components/admin-profile/areas/areas-form/areas-form.component.ts
+++ src/app/profile/components/admin-profile/areas/areas-form/areas-form.component.ts
@@ -1,19 +1,22 @@
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, AfterViewInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-areas-form',
   templateUrl: './areas-form.component.html',
   styleUrls: ['./areas-form.component.scss']
 })
-export class AreasFormComponent implements OnInit {
+export class AreasFormComponent implements OnInit, AfterViewInit {
 
-  constructor(private spinner: NgxSpinnerService , private router: Router ,
+  constructor(private spinner: NgxSpinnerService , private router: Router,
+    @Inject(DOCUMENT) private document: any, private elementRef: ElementRef,
     private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.loadScripts()
   }
 
 
@@ -29,5 +32,21 @@ export class AreasFormComponent implements OnInit {
 
   backToRoute(){
     this.router.navigateByUrl('/profile/en/admin/areas-defaults');
+  };
+
+
+  ngAfterViewInit(): void {
+    this.loadScripts();
+  }
+
+  loadScripts(){
+    let scripts = ['assets/scripts/custom.js'];
+
+    scripts.forEach(element => {
+      const s = this.document.createElement('script');
+      s.type = 'text/javascript';
+      s.src = element;
+      this.elementRef.nativeElement.appendChild(s);
+    });
   };
 }
--- src/app/profile/components/admin-profile/areas/areas-grid/areas-grid.component.ts
+++ src/app/profile/components/admin-profile/areas/areas-grid/areas-grid.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, Inject, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,11 +7,12 @@ import { Router } from '@angular/router';
   templateUrl: './areas-grid.component.html',
   styleUrls: ['./areas-grid.component.scss']
 })
-export class AreasGridComponent implements OnInit {
+export class AreasGridComponent implements OnInit, AfterViewInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router , @Inject(DOCUMENT) private document: any, private elementRef: ElementRef) { }
 
   ngOnInit() {
+    this.loadScripts();
   }
 
   pageChange(pageNumber){
@@ -19,5 +21,20 @@ export class AreasGridComponent implements OnInit {
 
   navigateToCreateNewArea(){
     this.router.navigate(['profile/en/admin/areas-action/new']);
+  };
+
+  ngAfterViewInit(): void {
+    this.loadScripts();
   }
+
+  loadScripts(){
+    let scripts = ['assets/scripts/custom.js'];
+
+    scripts.forEach(element => {
+      const s = this.document.createElement('script');
+      s.type = 'text/javascript';
+      s.src = element;
+      this.elementRef.nativeElement.appendChild(s);
+    });
+  };
 }
