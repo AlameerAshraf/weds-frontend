9defad26df1f886fa592eb2ae9a234e282ff7621
ArsanyNagy <arsany.nagy.hakim@gmail.com>
2021-04-03 20:59:55 +0200

    vendor

--- src/app/profile/components/vendor-profile/vendor-profile-details/vendor-profile-details.component.ts
+++ src/app/profile/components/vendor-profile/vendor-profile-details/vendor-profile-details.component.ts
@@ -1,4 +1,7 @@
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-vendor-profile-details',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendorProfileDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private spinner: NgxSpinnerService , private router: Router ,
+    private toastr: ToastrService) { }
 
   ngOnInit() {
   }
+
+  createNewCheckList(){
+    this.spinner.show();
+
+    setTimeout(() => {
+      this.spinner.hide();
+      this.toastr.success('Hello world!', 'Toastr fun!');
+      this.router.navigateByUrl('/profile/en/vendor/checklist-defaults');
+    }, 3000);
+  };
+
+  backToRoute(){
+    this.router.navigateByUrl('/profile/en/vendor/overview');
+  };
 
 }
