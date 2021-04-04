b46f650ac4c4a31e7517747e67d3b4b08a10ab87
alameerashraf <alamiir.ashraf@gmail.com>
2021-04-04 07:50:02 +0200

    Create the vendor details extra in vendor details and add overview/messages to the model

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -9,6 +9,7 @@ import { NgxDropzoneModule } from 'ngx-dropzone';
 import * as COMPONENTS from './components';
 import { AgmCoreModule } from '@agm/core';
 import { AngularEditorModule } from '@kolkov/angular-editor';
+import { VendorDetailsExtraComponent } from './components/vendor-profile/vendor-details-extra/vendor-details-extra.component';
 
 @NgModule({
   declarations: [
@@ -57,6 +58,7 @@ import { AngularEditorModule } from '@kolkov/angular-editor';
     COMPONENTS.EventDetailsComponent,
     COMPONENTS.CreateEventComponent,
     COMPONENTS.InviteFriendsComponent,
+    VendorDetailsExtraComponent,
   ],
   imports: [
     CommonModule,
