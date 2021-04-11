e1b5bf089108b29ff0a674e70ccee0063385743f
MoZaky <M.zaki.elkassed@gmail.com>
2021-04-11 03:51:41 +0300

    added RTL style

--- src/app/theme/components/header/header.component.ts
+++ src/app/theme/components/header/header.component.ts
@@ -1,38 +1,48 @@
-import { constants, resources } from 'src/app/core';
-import { Component, ElementRef, Inject, Input, OnInit, AfterViewInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { environment } from 'src/environments/environment';
-import { DOCUMENT } from '@angular/common';
+import { constants, resources } from "src/app/core";
+import {
+  Component,
+  ElementRef,
+  Inject,
+  Input,
+  OnInit,
+  AfterViewInit,
+} from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { environment } from "src/environments/environment";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
-  selector: 'weds-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "weds-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit, AfterViewInit {
   @Input() authorizedUser: boolean = false;
-  @Input() urlSegment = ''
-  @Input() type = ''
+  @Input() urlSegment = "";
+  @Input() type = "";
 
   isLogined = false;
   menuItems = [];
   profileLinks = [];
+
   lang: any;
 
   // Languge!
   loginURL: string;
   langChangerURL: string;
 
-
-  constructor(private actictedRoute: ActivatedRoute, private resources: resources,
+  constructor(
+    private actictedRoute: ActivatedRoute,
+    private resources: resources,
     @Inject(DOCUMENT) private document: any,
     private elementRef: ElementRef,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
   ngOnInit() {
     let currentUserEnc = window.localStorage.getItem("weds360#role");
     let currentUser = atob(currentUserEnc);
-
+    this.logOut();
     this.loadResources(currentUser);
     this.checkViewAuthority();
     this.loginURL = `/security/${this.lang}/login`;
@@ -42,26 +52,33 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   async loadResources(currentUserType) {
     let providedlang: any = this.actictedRoute.parent.params;
     let lang = providedlang._value["lang"];
-    let resourceLang = this.lang = ((lang == null) || (lang == undefined)) ? environment.defaultLang : lang;
+    let resourceLang = (this.lang =
+      lang == null || lang == undefined ? environment.defaultLang : lang);
 
     let dashboardNavs = `${currentUserType.toUpperCase()}_DASHBOARD`;
-    let resData = await this.resources.load(resourceLang, constants.VIEWS[dashboardNavs]) as any;
+    let resData = (await this.resources.load(
+      resourceLang,
+      constants.VIEWS[dashboardNavs]
+    )) as any;
     this.menuItems = this.menuItems.concat(resData.res["menu"]);
     this.profileLinks = this.profileLinks.concat(resData.res["actions"]);
-  };
+  }
 
-  checkViewAuthority(){
+  checkViewAuthority() {
     this.isLogined = this.authorizedUser;
-  };
+  }
 
   ngAfterViewInit(): void {
-    let scripts = ['assets/scripts/custom.js' , 'assets/scripts/changeLang.js'];
+    let scripts = ["assets/scripts/custom.js", "assets/scripts/changeLang.js"];
 
-    scripts.forEach(element => {
-      const s = this.document.createElement('script');
-      s.type = 'text/javascript';
+    scripts.forEach((element) => {
+      const s = this.document.createElement("script");
+      s.type = "text/javascript";
       s.src = element;
       this.elementRef.nativeElement.appendChild(s);
     });
-  };
+  }
+  logOut() {
+    window.localStorage.clear();
+  }
 }
