2972104b94cb853aebda1a634ef0662de484a9d6
MoZaky <M.zaki.elkassed@gmail.com>
2021-04-12 19:53:53 +0200

    style and homopage local

--- src/app/app-module-components/anonymous-home/anonymous-home.component.ts
+++ src/app/app-module-components/anonymous-home/anonymous-home.component.ts
@@ -1,64 +1,76 @@
-import { responseModel } from './../../core/models/response';
-import { urls } from './../../core/helpers/urls/urls';
-import { httpService } from './../../core/services/http/http';
-import { Component, OnInit } from '@angular/core';
-import { Inject, AfterViewInit, ElementRef } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
-import { constants, resources } from 'src/app/core';
-import { environment } from '../../../environments/environment';
-import { ActivatedRoute } from '@angular/router';
+import { responseModel } from "./../../core/models/response";
+import { urls } from "./../../core/helpers/urls/urls";
+import { httpService } from "./../../core/services/http/http";
+import { Component, OnInit } from "@angular/core";
+import { Inject, AfterViewInit, ElementRef } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { constants, resources } from "src/app/core";
+import { environment } from "../../../environments/environment";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './anonymous-home.component.html',
-  styleUrls: ['./anonymous-home.component.scss']
+  selector: "app-home",
+  templateUrl: "./anonymous-home.component.html",
+  styleUrls: ["./anonymous-home.component.scss"],
 })
 export class AnonymousHomeComponent implements OnInit, AfterViewInit {
   // Animation variables!
   startTypingAnimation: boolean = true;
 
   allCategories = [];
-
-  constructor(@Inject(DOCUMENT) private document: any,
-    private elementRef: ElementRef, private resources: resources,
+  //labels
+  labels: any = {};
+  constructor(
+    @Inject(DOCUMENT) private document: any,
+    private elementRef: ElementRef,
+    private resources: resources,
     private http: httpService,
-    private actictedRoute: ActivatedRoute) {
+    private actictedRoute: ActivatedRoute
+  ) {
     this.loadResources();
   }
 
-
   ngOnInit() {
     this.getAllCategories();
-  };
+  }
 
   /** Use this function at each view to load corrosponding resources! */
-  async loadResources(){
+  async loadResources() {
     let providedlang: any = this.actictedRoute.parent.params;
     let lang = providedlang._value["lang"];
-    let resourceLang = ((lang == null) || (lang == undefined)) ? environment.defaultLang : lang;
+    let resourceLang =
+      lang == null || lang == undefined ? environment.defaultLang : lang;
 
-    let resData = await this.resources.load(resourceLang , constants.VIEWS["HOME_LAYOUT"]);
-  };
+    let resData = (await this.resources.load(
+      resourceLang,
+      constants.VIEWS["HOME_LAYOUT"]
+    )) as any;
 
-  getAllCategories(){
+    if (!resData.error) {
+      this.labels = resData.res;
+    }
+  }
+
+  getAllCategories() {
     let allCatesURL = `${urls.GET_ALL_CATEGORIES}/${constants.APP_IDENTITY_FOR_USERS}`;
-    this.http.Get(allCatesURL , {}).subscribe((response: responseModel) => {
-      if(!response.error){
+    debugger;
+    this.http.Get(allCatesURL, {}).subscribe((response: responseModel) => {
+      if (!response.error) {
         this.allCategories = response.data;
       } else {
         console.log(response.error);
       }
     });
-  };
+  }
 
   ngAfterViewInit(): void {
-    let scripts = ['assets/scripts/typedwords.js' , 'assets/scripts/custom.js'];
+    let scripts = ["assets/scripts/typedwords.js", "assets/scripts/custom.js"];
 
-    scripts.forEach(element => {
-      const s = this.document.createElement('script');
-      s.type = 'text/javascript';
+    scripts.forEach((element) => {
+      const s = this.document.createElement("script");
+      s.type = "text/javascript";
       s.src = element;
       this.elementRef.nativeElement.appendChild(s);
     });
-  };
+  }
 }
