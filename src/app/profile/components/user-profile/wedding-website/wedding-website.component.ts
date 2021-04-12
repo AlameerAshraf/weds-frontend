83db5cc2f4391e5af625ae39a1d4ea8338578256
alameerashraf <alamiir.ashraf@gmail.com>
2021-04-12 08:59:16 +0200

    Load all theme templates and select a specific theme

--- src/app/profile/components/user-profile/wedding-website/wedding-website.component.ts
+++ src/app/profile/components/user-profile/wedding-website/wedding-website.component.ts
@@ -1,6 +1,10 @@
 import { MapsAPILoader } from '@agm/core';
 import { DOCUMENT } from '@angular/common';
 import { Component, OnInit, ViewEncapsulation, AfterViewInit, Inject, ElementRef, NgZone, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { constants, httpService, responseModel, theme, urls } from 'src/app/core';
 //import { } from '@types/googlemaps';
 declare const google: any
 
@@ -23,27 +27,45 @@ export class WeddingWebsiteComponent implements OnInit, AfterViewInit {
   files: File[] = [];
 
   private geoCoder: any;
+  currentUserEmail: string;
+
+
+  themesTemplates: theme[] = [];
 
   constructor(@Inject(DOCUMENT) private document: any,
     private elementRef: ElementRef, private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone) { }
+    private ngZone: NgZone, private router: Router, private http: httpService,
+    private ngxSpinner: NgxSpinnerService, private toastr: ToastrService) {
+    this.currentUserEmail = atob(window.localStorage.getItem("weds360#email"));
+  }
 
   ngOnInit() {
     this.mapsLoader();
+    this.loadThemesTemplates();
   }
 
-  selectTemplate(e: any) {
+  selectTemplate(e: any , templateId) {
     e.preventDefault();
-    this.is = !this.is;
-    let like = document.getElementById('template1');
-    if (!this.is) {
+
+    this.themesTemplates.forEach((aTheme) => {
+      aTheme.isThemeSelected = false;
+    });
+
+    let targetTemplate = this.themesTemplates.find(x => x._id == templateId);
+    targetTemplate.isThemeSelected = !targetTemplate.isThemeSelected;
+    let like = document.getElementById(templateId);
+    if (!targetTemplate.isThemeSelected) {
       like.classList.add("liked");
     } else {
       like.classList.remove("liked");
     }
   };
 
-  onFileSelected(e: any): void {
+  createNewWebsiteRequest() {
+
+  };
+
+  onCoverPhotoChanged(e: any): void {
     if (e.target.files && e.target.files[0]) {
       const imageFile = e.target.files[0];
       const fileReader = new FileReader();
@@ -54,7 +76,25 @@ export class WeddingWebsiteComponent implements OnInit, AfterViewInit {
     }
   };
 
-  mapsLoader(){
+
+  loadThemesTemplates(){
+    this.ngxSpinner.show();
+    let getAllThemes = `${urls.GET_ALL_THEMES}/${constants.APP_IDENTITY_FOR_USERS}`;
+
+    this.http.Get(getAllThemes , {}).subscribe((response: responseModel) => {
+      if(!response.error){
+        this.ngxSpinner.hide();
+        this.themesTemplates = response.data as theme[];
+      }else{
+        this.ngxSpinner.hide();
+        this.toastr.error("Our bad sorry!" , "My bad, server couldn't load any themes!");
+      }
+    });
+  };
+
+
+  //#region Address Helper Function..
+  mapsLoader() {
     this.mapsAPILoader.load().then(() => {
       this.setCurrentLocation();
       this.geoCoder = new google.maps.Geocoder;
@@ -85,7 +125,7 @@ export class WeddingWebsiteComponent implements OnInit, AfterViewInit {
     }
   };
 
-  markerDragEnd(e: any){
+  markerDragEnd(e: any) {
     this.latitude = e.coords.lat;
     this.longitude = e.coords.lng;
     this.getAddress(this.latitude, this.longitude);
@@ -105,8 +145,10 @@ export class WeddingWebsiteComponent implements OnInit, AfterViewInit {
       }
     });
   };
+  //#endregion
 
-  onSelect(event : any) {
+  //#region  DropZone Engine Helper Function..
+  onSelect(event: any) {
     console.log(event);
     this.files.push(...event.addedFiles);
   };
@@ -115,13 +157,15 @@ export class WeddingWebsiteComponent implements OnInit, AfterViewInit {
     console.log(event);
     this.files.splice(this.files.indexOf(event), 1);
   };
+  //#endregion
 
-  createNewWebsiteRequest(){
-
+  //#region Loading Proper Helper Function..
+  ngAfterViewInit(): void {
+    this.loadScripts();
   };
 
-  ngAfterViewInit(): void {
-    let scripts = ['assets/scripts/datePickerInitakizer.js', 'assets/scripts/custom.js' , 'assets/scripts/dropzone.js'];
+  loadScripts(){
+    let scripts = ['assets/scripts/datePickerInitakizer.js', 'assets/scripts/custom.js', 'assets/scripts/dropzone.js'];
 
     scripts.forEach(element => {
       const s = this.document.createElement('script');
@@ -130,4 +174,5 @@ export class WeddingWebsiteComponent implements OnInit, AfterViewInit {
       this.elementRef.nativeElement.appendChild(s);
     });
   };
+  //#endregion
 }
