ba0564491e2b814b79560ee4a8df575c665799cd
alameerashraf <alamiir.ashraf@gmail.com>
2021-04-19 00:56:11 +0200

    Adding lookups services for the all components Adding post model

---
+++ src/app/core/models/post.ts
@@ -0,0 +1,24 @@
+
+
+export class post {
+  titleEn?: string;
+  titleAr?: string;
+  subtitleEn?: string;
+  subtitleAr?: string;
+  category?: string;
+  tagsEn?: any[] = [];
+  tagsAr?: any[] = [];
+  isAuthorVisible?: boolean = false;
+  author?: string;
+  scheduledAt?: Date;
+  isScheduledPost?: boolean = false;
+  featuredImage?: string = "";
+  featuredVideo?: string = "";
+  bodyContentEn?: string;
+  bodyContentAr?: string;
+  metaDescriptionEn?: string;
+  metaDescriptionAr?: string;
+  readingTime?: number;
+  relatedPosts?: any[] = [];
+  images?: { url: string , arabicDesc: string, englishDesc: string }[] = [];
+}
