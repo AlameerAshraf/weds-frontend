230e69b1252cf0b5f1505591f0e97cb40dddb530
alameerashraf <alamiir.ashraf@gmail.com>
2021-04-25 12:59:36 +0200

    Bind vendor data in vendor view

--- src/app/segments-and-vendors/components/vendor/vendor.component.ts
+++ src/app/segments-and-vendors/components/vendor/vendor.component.ts
@@ -1,26 +1,36 @@
-import { urls, localStorageService, constants, httpService, post, responseModel, vendor } from 'src/app/core';
+import { urls, localStorageService, constants, httpService, post, responseModel, vendor , socialSharing, comment } from 'src/app/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ActivatedRoute } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
 import { Component, ElementRef, Inject, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-vendor',
   templateUrl: './vendor.component.html',
   styleUrls: ['./vendor.component.scss']
 })
 export class VendorComponent implements OnInit {
+
   isAuthed: boolean;
   vendorId: any;
   currentUserEmail: string;
 
-  telRef;
+  telRef: any;
+  mailTo: any;
+  userComment = new comment();
+  allComments: comment[] = [];
   vendor = new vendor();
+  facebookURL: string;
+  pinterestURL: string;
+  instagramURL: string;
+  twitterURL: string;
+  htmlView: any;
 
 
   constructor(@Inject(DOCUMENT) private document: any, private ActivatedRoute: ActivatedRoute,
     private toaster: ToastrService, private spineer: NgxSpinnerService, private httpService: httpService,
-    private elementRef: ElementRef, private localStorage: localStorageService) {
+    private elementRef: ElementRef, private localStorage: localStorageService , private _sanitizer: DomSanitizer) {
       this.currentUserEmail = atob(window.localStorage.getItem("weds360#email"));
 
       this.ActivatedRoute.params.subscribe((params) => {
@@ -31,6 +41,7 @@ export class VendorComponent implements OnInit {
 
   ngOnInit() {
     this.checkLoginStatus();
+    this.getAllComments();
   }
 
   loadVendorData(id: any){
@@ -41,13 +52,65 @@ export class VendorComponent implements OnInit {
       if(!response.error){
         this.spineer.hide();
         this.vendor = response.data as vendor;
-        this.telRef = `tel:${this.vendor.phone}`
+        this.telRef = `tel:${this.vendor.phone}`;
+        this.mailTo = `mailto:${this.vendor.email}`;
+
+        this.facebookURL = this.vendor.social.find(x => x.includes('facebook'));
+        this.instagramURL = this.vendor.social.find(x => x.includes('instagram'));
+        this.pinterestURL = this.vendor.social.find(x => x.includes('pinterest'));
+        this.twitterURL = this.vendor.social.find(x => x.includes('twitter'));
+
+        this.htmlView = this._sanitizer.bypassSecurityTrustHtml(this.vendor.descriptionURLAr);
+        this.vendor.avatar = this.vendor.avatar == undefined ? 'assets/images/defaults/avatar/vendor.png' : this.vendor.avatar;
+
+        console.log(this.vendor)
       }else{
         this.spineer.hide();
         this.toaster.error("Our bad sorry!" , "My bad, server couldn't load your budegeters.");
       }
     });
   };
+
+
+
+  //#region User Helpers..
+  rate(rateValue , rateCriteria){
+    console.log(rateValue , rateCriteria)
+  };
+
+
+  comment(){
+    this.userComment.userEmail = this.currentUserEmail;
+    let commentURL = `${urls.CREATE_NEW_COMMENT}/${constants.APP_IDENTITY_FOR_USERS}/${this.vendorId}`;
+    this.httpService.Post(commentURL , {} , { "comment" : this.userComment }).subscribe((response: responseModel) => {
+      if(!response.error){
+        this.spineer.hide();
+        this.getAllComments();
+        this.toaster.success("Gooood!" , `You review has been added, it will help people know ${this.vendor.nameEn} well! ❤`);
+      }else{
+        this.spineer.hide();
+        this.toaster.error("Our bad sorry!" , "My bad, Server couldn't create your comment, try again later.");
+      }
+    });
+  };
+
+  getAllComments(){
+    let getAllCommnetsURL = `${urls.GET_ALL_COMMENTS}/${constants.APP_IDENTITY_FOR_USERS}/${this.vendorId}`;
+    this.httpService.Get(getAllCommnetsURL , {}).subscribe((response: responseModel) => {
+      if(!response.error){
+        this.spineer.hide();
+        this.allComments = response.data as comment[];
+        this.allComments.forEach((aComment) => {
+          aComment.date = new Date(aComment.date).toDateString();
+          aComment.userAvatar = aComment.gender == "Female" ? 'assets/images/defaults/avatar/bride.png' : 'assets/images/defaults/avatar/groom.png';
+        });
+      }else{
+        this.spineer.hide();
+        this.toaster.error("Our bad sorry!" , "My bad, Server couldn't load vendor comments.");
+      }
+    })
+  }
+  //#endregion
 
 
   //#region Helper methods
@@ -58,6 +121,14 @@ export class VendorComponent implements OnInit {
     } else {
       this.isAuthed = false;
     }
+  };
+
+  openLink(url){
+    window.open(url);
+  };
+
+  share(type , url){
+    socialSharing.createNavigationUrl(type , url);
   };
 
   ngAfterViewInit(): void {
