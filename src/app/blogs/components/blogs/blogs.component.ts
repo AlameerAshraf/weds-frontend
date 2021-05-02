5549970aeb0c3f397b12be1f21a9cef9f80cc77a
alameerashraf <alamiir.ashraf@gmail.com>
2021-05-02 15:14:59 +0200

    Adding infinite scroll package and change routing to reload the page again

--- src/app/blogs/components/blogs/blogs.component.ts
+++ src/app/blogs/components/blogs/blogs.component.ts
@@ -1,3 +1,7 @@
+import { localStorageService } from 'src/app/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blogs.component.scss']
 })
 export class BlogsComponent implements OnInit {
+  categoryId;
+  isAuthed: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private Router: Router, private activatedRoute: ActivatedRoute,
+    private spinner: NgxSpinnerService, private localStorage: localStorageService ) {
+    this.activatedRoute.params.subscribe((params) => {
+      this.categoryId = params["categoryId"];
+    });
   }
 
+  ngOnInit() {
+    this.checkLoginStatus();
+  }
+
+  checkLoginStatus(){
+    let isLogined = this.localStorage.getLocalStorage("weds360#data");
+    if(isLogined != undefined || isLogined != ''){
+      this.isAuthed = true;
+    } else {
+      this.isAuthed = false;
+    }
+  };
 }
