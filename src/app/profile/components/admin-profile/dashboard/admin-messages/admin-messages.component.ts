c9a72f4b4b81919a35fac034f64b7df504ce7f2e
alameerashraf <alamiir.ashraf@gmail.com>
2021-05-07 02:34:45 +0200

    messages in the admin contacts

--- src/app/profile/components/admin-profile/dashboard/admin-messages/admin-messages.component.ts
+++ src/app/profile/components/admin-profile/dashboard/admin-messages/admin-messages.component.ts
@@ -1,33 +1,105 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, ElementRef, Inject, OnInit, AfterViewInit } from '@angular/core';
-import { constants, resources } from "src/app/core";
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { conatct, constants, httpService, resources, responseModel, urls } from "src/app/core";
 import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-admin-messages',
+  selector: 'app-vendor-messages',
   templateUrl: './admin-messages.component.html',
   styleUrls: ['./admin-messages.component.scss']
 })
-export class AdminMessagesComponent implements OnInit, AfterViewInit {
+export class AdminMessagesComponent implements OnInit {
+
   lang: string;
   labels: any = {};
-  constructor(@Inject(DOCUMENT) private document: any,
-    private elementRef: ElementRef,private resources: resources) { }
+  contacts: conatct[] = [];
+  currentUserEmail: string;
+
+  // Paging vars!
+  collectionSize: number = 0;
+  pageSize: any = 5;
+  limit: number;
+  skip: number;
+  showPaging = true;
+  // End paging vars!
+
+  constructor(private resources: resources, private router: Router, private http: httpService,
+    private toaster: ToastrService, private spinner: NgxSpinnerService) {
+      this.currentUserEmail = atob(window.localStorage.getItem("weds360#email"));
+  }
 
   ngOnInit() {
     this.loadResources();
+    this.getAllUserContacts();
   }
 
-  ngAfterViewInit(): void {
-    let scripts = ['assets/scripts/custom.js'];
+  getAllUserContacts(){
+    this.spinner.show();
+    let contactsURL = `${urls.GET_CONTACTS}/${constants.APP_IDENTITY_FOR_USERS}/${this.currentUserEmail}`;
+    this.http.Get(contactsURL , {}).subscribe((response: responseModel) => {
+      if (!response.error) {
+        this.spinner.hide();
+        this.contacts = response.data as conatct[];
+        this.collectionSize = this.contacts.length;
+        this.pageChange(1);
 
-    scripts.forEach(element => {
-      const s = this.document.createElement('script');
-      s.type = 'text/javascript';
-      s.src = element;
-      this.elementRef.nativeElement.appendChild(s);
-    });
+        this.contacts.forEach((aContact) => {
+          aContact.userAvatar = (aContact.userAvatar == "" || aContact.userAvatar == undefined)
+            ? "assets/images/defaults/avatar/vendor.png" : aContact.userAvatar;
+        });
+
+      } else {
+        this.spinner.hide();
+        this.toaster.error("Error loading your contacts", "Something wnet wrong on the server!");
+      }
+    })
   };
+
+
+  redierctToSingleChat(contactId){
+    this.spinner.show();
+    this.router.navigate([`profile/${this.lang}/vendor/chat`] , { queryParams: { 'dest' : contactId } })
+  };
+
+  timeSince(date: any) {
+
+    let currentDate = new Date() as any;
+    let compareDate = new Date(date) as any;
+
+    var seconds = Math.floor((currentDate - compareDate) / 1000);
+
+    var interval = seconds / 31536000;
+
+    if (interval > 1) {
+      return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+  };
+
+  pageChange(pageNumber){
+    window.scroll(0,0);
+    this.limit = this.pageSize * pageNumber;
+    this.skip = Math.abs(this.pageSize - this.limit);
+  };
+
   async loadResources() {
     let lang =
       window.location.href.toString().toLowerCase().indexOf("ar") > -1
@@ -37,11 +109,11 @@ export class AdminMessagesComponent implements OnInit, AfterViewInit {
     let resourceLang =
       lang == null || lang == undefined ? environment.defaultLang : lang;
     this.lang = resourceLang;
-
+    // alert(this.lang);
     let resData = (await this.resources.load(
       resourceLang,
       constants.VIEWS["MESSAGES"]
     )) as any;
     this.labels = resData.res;
-  }
+  };
 }
