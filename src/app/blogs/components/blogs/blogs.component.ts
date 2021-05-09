4710c930364f343eb270e5e6195f51efc80989f0
Mohamed Zaky <m.zaki.elkassed@gmail.com>
2021-05-09 02:57:23 +0200

    blog RTL and changed posts model

--- src/app/blogs/components/blogs/blogs.component.ts
+++ src/app/blogs/components/blogs/blogs.component.ts
@@ -13,13 +13,19 @@ export class BlogsComponent implements OnInit {
   categoryId;
   isAuthed: boolean;
   currentUserEmail: string;
-
+  lang: string;
   blogs: post[] = [];
   featuredPost = new post();
-
-  constructor(private Router: Router, private activatedRoute: ActivatedRoute,
+  // Paging vars!
+  collectionSize: number = 0;
+  pageSize: any = 9;
+  limit: number;
+  skip: number;
+  showPaging = true;
+  // End paging vars!
+  constructor(private router: Router, private activatedRoute: ActivatedRoute,
     private spinner: NgxSpinnerService, private localStorage: localStorageService,
-    private http: httpService, private toastr: ToastrService ) {
+    private http: httpService, private toastr: ToastrService) {
     this.currentUserEmail = atob(window.localStorage.getItem("weds360#email"));
 
     this.activatedRoute.params.subscribe((params) => {
@@ -28,29 +34,32 @@ export class BlogsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.lang = window.location.href.toString().toLowerCase().indexOf("ar") > -1 ? "ar" : "en";
     this.checkLoginStatus();
     this.getBlogsPercategory();
   }
 
 
-  getBlogsPercategory(){
+  getBlogsPercategory() {
     let blogsPerCategoryURL = `${urls.GET_ALL_BLOGS_PER_CATEGORY}/${constants.APP_IDENTITY_FOR_USERS}/${this.currentUserEmail}`;
 
-    this.http.Get(blogsPerCategoryURL , { "category": this.categoryId }).subscribe((response: responseModel) =>{
-      if(!response.error){
+    this.http.Get(blogsPerCategoryURL, { "category": this.categoryId }).subscribe((response: responseModel) => {
+      if (!response.error) {
         this.spinner.hide();
         this.blogs = response.data as post[];
         console.log(this.blogs)
+        this.collectionSize = this.blogs.length;
+        this.pageChange(1);
         this.featuredPost = this.getRandomPost(this.blogs);
         // this.featuredPost.publishedDate = this.featuredPost.publishedAt.toLocaleDateString();
-      }else{
+      } else {
         this.spinner.hide();
-        this.toastr.error("Our bad sorry!" , "My bad, server couldn't load any bost.");
+        this.toastr.error("Our bad sorry!", "My bad, server couldn't load any bost.");
       }
     })
   };
 
-  navigateToBlogPost(blogId){
+  navigateToBlogPost(blogId) {
 
   };
 
@@ -60,12 +69,21 @@ export class BlogsComponent implements OnInit {
     return item;
   }
 
-  checkLoginStatus(){
+  checkLoginStatus() {
     let isLogined = this.localStorage.getLocalStorage("weds360#data");
-    if(isLogined != undefined || isLogined != ''){
+    if (isLogined != undefined || isLogined != '') {
       this.isAuthed = true;
     } else {
       this.isAuthed = false;
     }
+  };
+  pageChange(pageNumber) {
+    window.scroll(0, 0);
+    this.limit = this.pageSize * pageNumber;
+    this.skip = Math.abs(this.pageSize - this.limit);
+  };
+  navigateToBlog(categoryName, blogId) {
+    categoryName = categoryName.replace(/ /g, "-");
+    this.router.navigate([`blogs/${this.lang}/blog/${categoryName}/${blogId}`]);
   };
 }
